7f3eb6a6eefed15f40d47170a9e38fb9f198d765
Kacper <kacper.tylec1999@gmail.com>
2024-01-26 12:53:11 +0100

    VoiceDetector images

--- App/ViewModels/VoiceDetectorViewModel.cs
+++ App/ViewModels/VoiceDetectorViewModel.cs
@@ -8,5 +8,69 @@ namespace App.ViewModels
 {
     public class VoiceDetectorViewModel : BaseViewModel
     {
+        public const string redMicrophoneImageSource = "greenmicrophone.svg";
+        public const string greenMicropHoneImageSource = "redmicrophone.svg";
+
+        private string imageSource = "redmicrophone.svg";
+        private bool isStarted = false;
+        private bool isCompleted = false;
+
+        public string ImageSource
+        {
+            get { return imageSource; }
+            set
+            {
+                if (imageSource != value)
+                {
+                    imageSource = value;
+                    OnPropertyChanged(nameof(ImageSource));
+
+                }
+            }
+        }
+        public bool IsStarted
+        {
+            get { return isStarted; }
+            set
+            {
+                if (isStarted != value)
+                {
+                    isStarted = value;
+                    OnPropertyChanged(nameof(IsStarted));
+
+                }
+            }
+        }
+        public bool IsCompleted
+        {
+            get { return isCompleted; }
+            set
+            {
+                if (isCompleted != value)
+                {
+                    isCompleted = value;
+                    OnPropertyChanged(nameof(IsCompleted));
+
+                }
+            }
+        }
+
+
+
+        private string displayedInformation;
+
+
+        public Command StartDetecting { get; set; }
+        public Command StopDetecting { get; set; }
+
+        public Command EditSavedText { get; set; }
+
+        public VoiceDetectorViewModel()
+        {
+            StartDetecting = new Command(async () =>
+            {
+                ImageSource = greenMicropHoneImageSource;
+            });
+        }
     }
 }
