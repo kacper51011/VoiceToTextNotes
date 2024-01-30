9066533209a1fd5bd619f8c0a7cdf0a12da20918
Kacper <kacper.tylec1999@gmail.com>
2024-01-30 20:47:13 +0100

    Improved popup design

--- App/Views/CreateNoteFromVoicePopUp.xaml.cs
+++ App/Views/CreateNoteFromVoicePopUp.xaml.cs
@@ -8,9 +8,11 @@ public partial class CreateNoteFromVoicePopUp : Popup
 	public CreateNoteFromVoicePopUp(string content)
 	{
 		InitializeComponent();
+
+
         ContentEditor.Text = content;
 	}
-
+    
     private void Accept_Clicked(object sender, EventArgs e)
     {
         var obj = new NoteInformation {Content = ContentEditor.Text, Name = NameEntry.Text, Created= DateTime.Now };
